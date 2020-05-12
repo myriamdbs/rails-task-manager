19281e5eb4e94d0d42035cb0201e311afe9d219f
Myriam Dubois <myriam.dubois@skema.edu>
2020-05-12 15:03:44 +0200

    edit and patch route created, controller updated, edit view created, functionnality edit working on title, details and completed

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -17,9 +17,20 @@ class TasksController < ApplicationController
     redirect_to task_path(@task)
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    params[:completed] == "1" ? params[:completed] = true : params[:completed] = false
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
   private
 
   def task_params
-    params.require(:task).permit(:title, :details)
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
